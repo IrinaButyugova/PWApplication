4eceff64ae4e438febfc674d0c852d4357bf99a9
IrinaButyugova <irina.butyugova@sharp-dev.net>
2023-10-31 08:03:16 +0300

    Transaction creation

---
+++ PWApplication/PWBlazorApplication/Models/CreateTransactionModel.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace PWBlazorApplication.Models
+{
+    public class CreateTransactionModel
+    {
+        public List<string> Users { get; set; }
+
+        [Required]
+        public string RecipientName { get; set; }
+
+        [Required]
+        [DataType(DataType.Currency)]
+        public decimal Amount { get; set; }
+    }
+}
