6d0a895be1daff3c045dfb2768b714c65ec43e5b
IrinaButyugova <irina.butyugova@sharp-dev.net>
2023-11-24 11:25:49 +0300

    System changes state one time

--- PWApplication/PWBlazorApplication/Store/LoginUseCase/LoginReducers.cs
+++ PWApplication/PWBlazorApplication/Store/LoginUseCase/LoginReducers.cs
@@ -6,13 +6,6 @@ namespace PWBlazorApplication.Store.LoginUseCase
     public class LoginReducers
     {
         [ReducerMethod]
-        public static LoginState ReduceCheckSignInAction(LoginState state, CheckSignInAction action)
-        {
-            var newState = new LoginState(false, new List<Error>());
-            return newState;
-        }
-
-        [ReducerMethod]
         public static LoginState ReduceCheckSignInResultAction(LoginState state, CheckSignInResultAction action)
         {
             var newState = new LoginState(action.Result.Succeeded, action.Result.Errors);
