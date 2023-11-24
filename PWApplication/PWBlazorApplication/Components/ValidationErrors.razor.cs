bf60b150575b007dd454aee5920fc75837716535
IrinaButyugova <irina.butyugova@sharp-dev.net>
2023-11-24 10:04:30 +0300

    ValidationErrors component

---
+++ PWApplication/PWBlazorApplication/Components/ValidationErrors.razor.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.AspNetCore.Components;
+
+namespace PWBlazorApplication.Components
+{
+	public partial class ValidationErrors
+    {
+		[Parameter]
+		public List<string> Errors { get; set; } = new List<string>();
+	}
+}
