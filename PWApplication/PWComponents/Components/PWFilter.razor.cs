35b0d8a6fda0dcf987bd5fe60230bcdb56344b98
IrinaButyugova <irina.butyugova@sharp-dev.net>
2023-11-28 12:50:07 +0300

    Option to clear the filter

--- PWApplication/PWComponents/Components/PWFilter.razor.cs
+++ PWApplication/PWComponents/Components/PWFilter.razor.cs
@@ -10,5 +10,15 @@ namespace PWComponents.Components
 
 		[Parameter]
 		public EventCallback OnClickCallback { get; set; }
+
+		private async Task Clear()
+		{
+			FilterModel.StartDate = null;
+			FilterModel.EndDate = null;
+			FilterModel.CorrespondentName = String.Empty;
+			FilterModel.StartAmount = null;
+			FilterModel.EndAmount = null;
+			await OnClickCallback.InvokeAsync();
+		}
 	}
 }
